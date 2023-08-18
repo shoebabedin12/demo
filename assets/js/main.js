fc134a336827815992b75cf6d8cbd342fc50a70d
shoeb abedin <shoebabedin12@gmail.com>
2023-08-18 21:43:20 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,6 +54,46 @@ $(document).ready(function () {
         },
       },
   });
+  // we-develop
+  var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 3000, // Set the delay between slides in milliseconds
+      disableOnInteraction: false // Allow autoplay to continue even when the user interacts with the slider
+    },
+    loop: false, // Set to true for infinite loop
+    speed: 1000, // Adjust the speed value to control the slide transition speed
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        1024: {
+          slidesPerView: 4,
+          slidesPerGroup: 1,
+        },
+        769: {
+          slidesPerView: 3,
+          slidesPerGroup: 1,
+          spaceBetween: 16,
+        },
+        600: {
+          slidesPerView: 2,
+          slidesPerGroup: 1,
+          spaceBetween: 16,
+        },
+        300: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          spaceBetween: 16,
+        },
+      },
+  });
   // email sending
   document
     .getElementById("emailForm")
